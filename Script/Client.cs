9bf5fcf813c3f9b58b5c43d1340f936e8865ebc8
Phạm Huỳnh Tín <116443704+PhamHuynhTin@users.noreply.github.com>
2023-02-13 16:46:33 +0700

    Create Server 13/02/2023

---
+++ Script/Client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public int index;
+        public string IP;
+        public TcpClient Socket;
+        public NetworkStream myStream;
+        public byte[] readBuff;
+
+        public void Start()
+        {
+            Socket.SendBufferSize = 4096;
+            Socket.ReceiveBufferSize = 4096;
+            myStream = Socket.GetStream();
+            Array.Resize(ref readBuff, Socket.ReceiveBufferSize);
+            myStream.BeginRead(readBuff, 0, Socket.ReceiveBufferSize, OnReceiceData, null);
+        }
+
+        void CloseConnection()
+        {
+            Socket.Close();
+            Socket = null;
+            Console.WriteLine("Player disconnected: " + IP);
+        }
+
+        void OnReceiceData(IAsyncResult result)
+        {
+            try
+            {
+                int readBytes = myStream.EndRead(result);
+                if(Socket == null)
+                {
+                    return;
+                }
+                if(readBytes <= 0)
+                {
+                    CloseConnection();
+                    return;
+                }
+                byte[] newBytes = null;
+                Array.Resize(ref newBytes, readBytes);
+                Buffer.BlockCopy(readBuff, 0, newBytes, 0, readBytes);
+
+                if(Socket == null)
+                {
+                    return;
+                }
+                myStream.BeginRead(readBuff, 0, Socket.ReceiveBufferSize, OnReceiceData, null);
+            }
+            catch(Exception ex)
+            {
+                CloseConnection();
+                return;
+            }
+        }
+    }
+}
---
+++ Script/NetWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class NetWork
+    {
+        public TcpListener ServerSocket;
+        public static NetWork instance = new NetWork();
+        public static Client[] Clients = new Client[100];
+
+        public void ServerStart()
+        {
+            for(int i = 1; i < 100; i++)
+            {
+                Clients[i] = new Client();
+            }
+            ServerSocket = new TcpListener(IPAddress.Any, 5012);
+            ServerSocket.Start();
+            ServerSocket.BeginAcceptTcpClient(OnClientConnect ,null);
+            Console.WriteLine("Server has successfully started.");
+        }
+
+        void OnClientConnect(IAsyncResult result)
+        {
+            TcpClient client = ServerSocket.EndAcceptTcpClient(result);
+            client.NoDelay = false;
+            ServerSocket.BeginAcceptTcpClient(OnClientConnect, null);
+
+        }
+    }
+}
